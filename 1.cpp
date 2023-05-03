d935b83bf3665c883848e74221d1777c71a15848
Matvey-burkov <112969099+Matvey-burkov@users.noreply.github.com>
2023-05-03 11:52:48 +0700

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <string>
 
-
 class Category {
 public:
     int id;
@@ -37,14 +36,14 @@ public:
 class Product {
 public:
     int id;
-    std::string  seller_id;
+    int  id_seller;
     std::string  brand_id;
     std::string category_id;
     std::string name;
-    Product(int id , Seller*  seller_id, Brand*  brand_id, Category*  category_id,std::string name) {
+    Product(int id , int id_seller, Brand*  brand_id, Category*  category_id,std::string name) {
         this->name = name;
         this->id = id;
-        this->seller_id = seller_id->name;
+        this->id_seller = id_seller;
         this->brand_id = brand_id->name;
         this->category_id =category_id->name;
     }
@@ -60,6 +59,7 @@ std::string convert(std::string res) {
     return res;
 }
 
+std::string findName(int id,  Seller** sellers);
 
 int main() {
     setlocale(LC_ALL, "Russian");
@@ -82,17 +82,32 @@ int main() {
     Seller* seller4 = new Seller( 4, "Target" );
     Seller* seller5 = new Seller( 5, "Costco" );
 
-    Product* product1 = new Product( 1, seller1, brand1, category4, "обувь" );
-    Product* product2 = new Product( 2, seller2, brand2, category1, "iPhone" );
-    Product* product3 = new Product( 3, seller3, brand3, category3, "холодос" );
-    Product* product4 = new Product( 4, seller4, brand4, category2, "майка" );
-    Product* product5 = new Product( 5, seller5, brand5, category5, "шампунь" );
-
+    Product* product1 = new Product( 1, 1, brand1, category4, "обувь" );
+    Product* product2 = new Product( 2, 2, brand2, category1, "iPhone" );
+    Product* product3 = new Product( 3, 1, brand3, category3, "холодос" );
+    Product* product4 = new Product( 4, 3, brand4, category2, "майка" );
+    Product* product5 = new Product( 5, 1, brand5, category5, "шампунь" );
+    
+    Seller* sellers[] = {seller5, seller4, seller3, seller2, seller1};
     std::cout << convert("ID") << convert("Имя") << convert("Продавец") << convert("Бренд") << convert("Категория") << std::endl;
     Product* Array[] = { product1 ,product2 ,product3 ,product4 ,product5 };
-    for (auto i : Array) {
-        std::cout << convert(std::to_string(i->id))<<convert(i->name) << convert(i->seller_id)<< convert(i->brand_id)<< convert(i->category_id) << '\n';
-    }
+    // for (auto i : Array) {
+    //     std::cout << convert(std::to_string(i->id))<<convert(i->name) << convert(i->seller_id)<< convert(i->brand_id)<< convert(i->category_id) << '\n';
+    // }
+    
+    std::cout << findName(product5->id_seller, sellers);
+    
+    
    
     return 0;
+}
+
+std::string findName(int id,  Seller** sellers) {
+    for (int i = 0; i < 5; i++) {
+        if (id == sellers[i]->id) {
+            std::string  name = sellers[i]->name;
+            return name;
+    }
+    }
+    return NULL;
 }
